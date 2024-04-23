ac39536adfa424e844bb90b2ea6e2ee5ebaaa4c6
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-23 09:59:52 +0200

    files organizations

--- classes/db_class.php
+++ classes/db_class.php
@@ -87,66 +87,7 @@ class Database {
         }
     }
 
-    public function getAllUsers()
-    {
-        $query = "SELECT * FROM users";
 
-        try
-        {
-            $statement = $this->connection->prepare($query);
-            $res = $statement->execute();
-            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $rows;
-        }
-        catch(PDOException $e)
-        {
-            return "Error";
-        }
-
-
-    }
-
-    public function findOneUser($email,$password)
-    {
-        $query = "SELECT * FROM users WHERE email= :email AND password= :password";
-        $statement = $this->connection->prepare($query);
-        $statement->bindParam(':email',$email);
-        $statement->bindParam(':password',$password);
-
-        try
-        {
-            $statement->execute();
-            if($statement->rowCount() == 1)
-            {
-                echo "User found!";
-                return true;
-            }
-            echo "User not found!";
-            return false;
-        }
-        catch (PDOException $e)
-        {
-            return false;
-        }
-    }
-
-    public function getUserById($id)
-    {
-        $query = "SELECT * FROM users WHERE id = :id";
-        $statement = $this->connection->prepare($query);
-        $statement->bindParam(':id',$id);
-
-        try
-        {
-            $statement->execute();
-            $user = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $user;
-        }   
-        catch (PDOException)
-        {
-            return "Error";
-        }
-    }
     public function __destruct() {
         $this->connection = null;
     }
