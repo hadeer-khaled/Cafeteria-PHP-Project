e6f98486c27b1ec1b565bc5b217266ca1b3a7774
esraamagddi <esraamagdy20133@gmail.com>
2024-04-24 21:37:54 +0200

    setting up helpers classes

---
+++ App.php
@@ -0,0 +1,15 @@
+<?php
+use classes\{  
+    Request,
+    Session
+};
+
+use validation\Validator;
+
+require_once 'classes/helpers/Request.php';
+require_once 'classes/helpers/Session.php';
+
+
+
+$request= new Request;
+$session=new Session;
---
+++ classes/helpers/Request.php
@@ -0,0 +1,38 @@
+<?php
+namespace helpers;
+
+class Request{
+
+    public function post($key)
+    {
+        return (isset($_POST[$key])? $_POST[$key] :FALSE);
+
+    }
+
+    public function get($key)
+    {
+        return (isset($_GET[$key])? $_GET[$key] :FALSE);
+    }
+
+    public function redirect($path)
+    {
+        return header("location:$path");
+    }
+
+    public function file($key)
+    {
+        return (isset($_FILES[$key])? $_FILES[$key] :FALSE);
+    }
+
+    public function ispost()
+    {
+        return ($_SERVER['REQUEST_METHOD']=='POST'? true :FALSE);
+
+    }
+
+    public function isget()
+    {
+        return ($_SERVER['REQUEST_METHOD']=='GET'? true :FALSE);
+
+    }
+}
